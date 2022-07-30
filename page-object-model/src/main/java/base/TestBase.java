6d820a4e90a5261b43d706bdf1e44992fabfd38b
Jan_Balangue <jan.a.balangue@gmail.com>
2022-07-30 12:28:23 -0500

    Add drivers and TestBase, change xml files

---
+++ page-object-model/src/main/java/base/TestBase.java
@@ -0,0 +1,29 @@
+package base;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+	private static WebDriver driver;
+
+	@BeforeSuite
+	public void setup() {
+		File chromeFile = new File(System.getProperty("user.dir") + "/src/main/java/drivers/chromedriver");
+		System.setProperty("webdriver.chrome.driver", chromeFile.getAbsolutePath());
+		File firefoxFile = new File(System.getProperty("user.dir") + "/src/main/java/drivers/geckodriver");
+		System.setProperty("webdriver.gecko.driver", firefoxFile.getAbsolutePath());
+		driver = new ChromeDriver();
+		driver.get("https://parabank.parasoft.com/parabank/index.htm");
+		driver.manage().window().maximize();
+	}
+
+	@AfterSuite
+	public void teardown() {
+		driver.quit();
+	}
+}
