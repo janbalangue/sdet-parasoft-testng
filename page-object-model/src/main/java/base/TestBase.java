80d47c6a3d80098535ab240560cb0a79df31e8e7
Jan_Balangue <jan.a.balangue@gmail.com>
2022-07-30 12:40:21 -0500

    change TestBase

--- page-object-model/src/main/java/base/TestBase.java
+++ page-object-model/src/main/java/base/TestBase.java
@@ -7,12 +7,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 public class TestBase {
 
 	private static WebDriver driver;
 
 	@BeforeSuite
+	@Test
 	public void setup() {
 		File chromeFile = new File(System.getProperty("user.dir") + "/src/main/java/drivers/chromedriver");
 		System.setProperty("webdriver.chrome.driver", chromeFile.getAbsolutePath());
@@ -25,6 +27,7 @@ public class TestBase {
 	}
 
 	@AfterSuite
+	@Test
 	public void teardown() {
 		driver.quit();
 		Reporter.log("driver quit");
